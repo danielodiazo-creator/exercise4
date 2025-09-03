40e9ca762f4a2b499d440dea3236c52748c7bf30
Daniel Diaz <danielodiazo@gmail.com>
2025-09-03 16:54:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,10 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
-            Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
-            Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
-            Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
-            Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
+            
         }
     }
 }
+
