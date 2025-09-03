755d899130190d24fb2d50c4c4d34a43e00246aa
Daniel Diaz <danielodiazo@gmail.com>
2025-09-03 16:46:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VSÖvning4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo. Este es el ejercicio eller övning 4");
+        }
+    }
+}
